27815529a377448714fd37c0af2a5d540ce28317
moohabir <87937848+moohabir@users.noreply.github.com>
2022-05-20 00:31:48 +0200

    x

--- src/Review.js
+++ src/Review.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { FaChevronRight, FaChevronLeft } from "react-icons";
+import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
 import data from "./data";
 
 export default function Review() {
