db57aa1c938b0bef13aa82ca4fa7c938ce8448de
moohabir <87937848+moohabir@users.noreply.github.com>
2022-05-20 19:19:26 +0200

    g

--- src/Review.js
+++ src/Review.js
@@ -1,33 +1,44 @@
 import { useState } from "react";
-import { FaChevronRight, FaChevronLeft } from "react-icons/fa";
+import { FaChevronRight, FaChevronLeft, FaQuoteRight } from "react-icons/fa";
 import data from "./data";
 
 export default function Review() {
-  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+  const { name, info, image } = data[index];
+
+  function next() {
+    setIndex(index + 1);
+  }
+
+  function previous() {
+    setIndex(index - 1);
+  }
 
   return (
-    <div className="review">
-      {people.map((person) => {
-        const { id, name, info, image } = person;
-        return (
-          <article key={id} {...person}>
-            <img src={image} alt={name} />
-            <div>
-              <h4>{name}</h4>
-              <p>{info}</p>
-            </div>
-          </article>
-        );
-      })}
-      {/*<div>
-        <button>
-          <FaChevronLeft />
-        </button>
-        <button>
-          {" "}
-          <FaChevronRight />
-        </button>
-      </div> */}
-    </div>
+    <main className="review">
+      <section>
+        <article className="article">
+          <div>
+            <img src={image} alt={name} className="image" />
+            <span>
+              <FaQuoteRight />
+            </span>
+          </div>
+          <div>
+            <h4>{name}</h4>
+            <p>{info}</p>
+          </div>
+        </article>
+
+        <div>
+          <button>
+            <FaChevronLeft onClick={previous} />
+          </button>
+          <button>
+            <FaChevronRight onClick={next} />
+          </button>
+        </div>
+      </section>
+    </main>
   );
 }
